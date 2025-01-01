c200ea9112cad5bec1df2e783fb78aaa42ec40bb
moizmalik13588 <moizm12348@gmail.com>
2025-01-01 16:54:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import connectDB from "./src/db/index.js"
+import userRoutes from "./src/routes/user.routes.js"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+app.use(cookieParser())
+app.use(express.json())
+
+app.use('/api/v1' , userRoutes)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!! ", err);
+  });
