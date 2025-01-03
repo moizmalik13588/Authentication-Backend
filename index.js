8387cab8a4662c256770d480dc26406920c2af58
moizmalik13588 <moizm12348@gmail.com>
2025-01-03 14:35:26 +0500

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import connectDB from "./src/db/index.js"
-import userRoutes from "./src/routes/user.routes.js"
-import dotenv from "dotenv"
+import express from "express";
+import cookieParser from "cookie-parser";
+import connectDB from "./src/db/index.js";
+import userRoutes from "./src/routes/user.routes.js";
+import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cookieParser())
-app.use(express.json())
+// Middleware
+app.use(cookieParser());
+app.use(express.json());
 
-app.use('/api/v1' , userRoutes)
+// CORS Configuration
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Frontend URL
+    credentials: true,              // Allow cookies and headers
+  })
+);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// Routes
+app.use("/api/v1", userRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+// Database Connection and Server Start
 connectDB()
   .then(() => {
     app.listen(process.env.PORT, () => {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,21 @@
-import express from "express"
-import { authenticateUser, loginUser, logoutUser, regenerateAccessToken, registerUser } from "../controllers/user.controllers.js";
+import express from "express";
+import {
+  authenticateUser,
+  loginUser,
+  logoutUser,
+  regenerateAccessToken,
+  registerUser,
+} from "../controllers/user.controllers.js";
 
 const router = express.Router();
 
-router.post('/register', registerUser)
-router.post('/login', loginUser)
-router.post('/logout', logoutUser)
-router.post('/generatetoken', regenerateAccessToken)
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/generatetoken", regenerateAccessToken);
 
-router.get('/userdata', authenticateUser, (req, res) => {
-    res.send("you are getting all user detail")
-})
+router.get("/userdata", authenticateUser, (req, res) => {
+  res.send("you are getting all user detail");
+});
 
-
-export default router
+export default router;
